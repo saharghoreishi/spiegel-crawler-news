6d326e5b2405cb182f8fd809f04477996393e7d0
saharghoreishi <35759004+saharghoreishi@users.noreply.github.com>
2019-10-16 19:11:10 +0330

    Add files via upload

--- crawl.py
+++ crawl.py
@@ -1,8 +1,13 @@
 import sqlite3
 import spiegel
+from apscheduler.schedulers.background import BackgroundScheduler
 
-spiegel.Crawl()
-conn = sqlite3.connect("sample.db")
+scheduler = BackgroundScheduler()
+scheduler.start()
+scheduler.add_job(spiegel.Crawl(), trigger='cron', second=60)
+
+
+conn = sqlite3.connect("sample.db")#to see what inserted in sqllite
 c = conn.cursor()
 sql = '''SELECT * FROM Spiegeln '''
 for company in c.execute(sql):
